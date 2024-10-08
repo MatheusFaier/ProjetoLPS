4bd0ee78048e3c4373d1ad9b863e801befaa4528
Matheus Faier <matheusalcfaier@gmail.com>
2024-10-08 14:48:15 -0300

    Teste

---
+++ ProjetoLPS/src/main/java/com/mycompany/projetolps/ProjetoLPS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetolps;
+
+/**
+ *
+ * @author mathe
+ */
+public class ProjetoLPS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
